72832d9f25888d5efff313ac43b1b85b385ef3da
brad lumber <codedevbrad@gmail.com>
2020-10-31 15:01:37 +0000

    initial commit

---
+++ spec/terminal-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TerminalView from '../lib/terminal-view';
+
+describe('TerminalView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
